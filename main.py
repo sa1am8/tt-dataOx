f4e76cbbb530a8ccfc48d6e095705e07b5ce17a4
Toshka <tinkerino571@gmail.com>
2022-02-23 21:31:26 +0200

    working prototype

--- main.py
+++ main.py
@@ -5,15 +5,15 @@ from objects import *
 
 class Game:
     def __init__(self):
-        self.elevator = Elevator(random.randint(5, 20), 0, 0)
+        self.elevator = Elevator(random.randint(5, 10), 0, 0)
         self.floors_len = len(self.elevator.floors)
 
         for i in range(self.floors_len):
-            self.elevator.floors[i] = [Human(random.choice([k for k in range(1, self.floors_len) if k != i + 1]))
+            self.elevator.floors[i] = [Human(random.choice([k for k in range(0, self.floors_len-1) if k != i + 1]))
                                        for j in range(random.randint(0, 5))]
         #what if 0 humans at the first floor?
         if len(self.elevator.floors[0]) == 0:
-            self.elevator.floors[0] += [Human(self.floors_len)]
+            self.elevator.floors[0] += [Human(self.floors_len-1)]
         self.step = 1
         self.elevator.goal = max([i.to_floor for i in self.elevator.floors[0]])
         self.elevator.inside = self.elevator.floors[0]
@@ -21,9 +21,14 @@ class Game:
         print(self.elevator)
 
     def play(self):
-        self.elevator.up()
-        print(self.elevator)
-
+        for i in range(10):
+            print('--------------------')
+            if self.elevator.goal > self.elevator.now:
+                self.elevator.up()
+            else:
+                self.elevator.down()
+            print(self.elevator)
+            print('--------------------')
 
 
 game = Game()
--- objects.py
+++ objects.py
@@ -13,6 +13,7 @@ class Human:
 
 
 class Elevator:
+
     def __init__(self, floors: list, goal: int, inside: list, now=0):
         self.floors = [[] for i in range(floors)]
         self.goal = goal
@@ -20,25 +21,62 @@ class Elevator:
         self.now = now
 
     def __repr__(self):
-        return f"{self.floors} | {str(self.inside)} to {str(self.goal)}"
+        return f"{self.floors} | {str(self.inside)} now at {str(self.now)} to {str(self.goal)}"
 
     def up(self):
         self.now += 1
-        people_now = self.floors[self.now] # позволяю тем, кто выйдет из лифта, сразу ехать дальше
+        people_now = self.floors[self.now]  # позволяю тем, кто выйдет из лифта, сразу ехать дальше
 
         for index, man in enumerate(self.inside):
             if man.to_floor == self.now:
                 print(man, 'came out')
-                people_now.append(man)
                 del self.inside[index]
-                people_now.append(Human(random.choice([k for k in range(1, self.floors_len) if k != self.now + 1])))
+                people_now.append(
+                    Human(random.choice([k for k in range(0, len(self.floors) - 1) if k != self.now + 1])))
 
         if people_now:
-            if len(self.inside) == maximum:
-                return
-            people_now.sort()
+
+            people_now.sort(key=lambda x: x.to_floor)
+            to_del = list()
             for index, man in enumerate(people_now):
-                if man.to_floor < self.goal:
+
+                if len(self.inside) == maximum:
+                    break
+
+                if man.to_floor > self.now:
                     print(man, 'came in')
                     self.inside += [man]
-                    del people_now[index]
+                    to_del += [man]
+                    self.goal = max(man.to_floor, self.goal)
+
+            for i in to_del:
+                people_now.remove(i)
+
+    def down(self):
+        self.now -= 1
+        people_now = self.floors[self.now]
+
+        for index, man in enumerate(self.inside):
+            if man.to_floor == self.now:
+                print(man, 'came out')
+                del self.inside[index]
+                people_now.append(
+                    Human(random.choice([k for k in range(0, len(self.floors) - 1) if k != self.now + 1])))
+
+        if people_now:
+
+            people_now.sort(key=lambda x: x.to_floor)
+            to_del = list()
+            for index, man in enumerate(people_now):
+
+                if len(self.inside) == maximum:
+                    break
+
+                if man.to_floor < self.now:
+                    print(man, 'came in')
+                    self.inside += [man]
+                    to_del += [man]
+                    self.goal = min(man.to_floor, self.goal)
+
+            for i in to_del:
+                people_now.remove(i)
